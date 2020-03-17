b72a3184d4c48501449f6356896f0f221399747c
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-03-17 01:00:17 -0300

    implements Price input unit test

--- src/PriceCalculator.test.js
+++ src/PriceCalculator.test.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react'
 import PriceCalculator from './PriceCalculator';
 
-it('renders correctly', () => {
-  const { queryByTestId } = render(<PriceCalculator />);
+const { queryByTestId } = render(<PriceCalculator />);
 
-  expect(queryByTestId('amount')).toBeTruthy();
-  expect(queryByTestId('price')).toBeTruthy();
+const amountInput = queryByTestId('amount');
+const priceInput = queryByTestId('price');
+
+it('renders correctly', () => {
+  expect(amountInput).toBeTruthy();
+  expect(priceInput).toBeTruthy();
+});
+
+describe('Price input value', () => {
+  fireEvent.change(amountInput, { target: { value: 7 } });
+
+  expect(amountInput.value).toBe('7');
+  expect(priceInput.value).toBe('70');
 });
