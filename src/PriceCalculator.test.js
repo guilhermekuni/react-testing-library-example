b6e15d1f13a899348f820c0e2825bcdc1c786f87
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-03-18 13:24:41 -0300

    adjusts tests

--- src/PriceCalculator.test.js
+++ src/PriceCalculator.test.js
@@ -2,39 +2,54 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react'
 import PriceCalculator from './PriceCalculator';
 
-const { queryByTestId } = render(<PriceCalculator />);
+describe('price calculator test', () => {
+  it('renders correctly', () => {
+    const { queryByTestId } = render(<PriceCalculator />);
 
-const amountInput = queryByTestId('amount');
-const coupomAmountInput = queryByTestId('coupomAmount');
-const priceInput = queryByTestId('price');
+    expect(queryByTestId('amount')).toBeTruthy();
+    expect(queryByTestId('coupomAmount')).toBeTruthy();
+    expect(queryByTestId('price')).toBeTruthy();
+  });
 
-it('renders correctly', () => {
-  expect(amountInput).toBeTruthy();
-  expect(coupomAmountInput).toBeTruthy();
-  expect(priceInput).toBeTruthy();
-});
+  it('calculate price input value', () => {
+    const { queryByTestId } = render(<PriceCalculator />);
 
-describe('price input value', () => {
-  fireEvent.change(amountInput, { target: { value: 12 } });
+    const amountInput = queryByTestId('amount');
+    const priceInput = queryByTestId('price');
 
-  expect(amountInput.value).toBe('12');
-  expect(priceInput.value).toBe('120');
-});
+    fireEvent.change(amountInput, { target: { value: 12 } });
 
-describe('price input value with coupom', () => {
-  fireEvent.change(amountInput, { target: { value: 7 } });
-  fireEvent.change(coupomAmountInput, { target: { value: 3 } });
+    expect(amountInput.value).toBe('12');
+    expect(priceInput.value).toBe('120');
+  });
 
-  expect(amountInput.value).toBe('7');
-  expect(coupomAmountInput.value).toBe('3');
-  expect(priceInput.value).toBe('55');
-});
+  it('calculate price input value with coupom', () => {
+    const { queryByTestId } = render(<PriceCalculator />);
 
-describe('price input with coupom value > total value', () => {
-  fireEvent.change(amountInput, { target: { value: 1 } });
-  fireEvent.change(coupomAmountInput, { target: { value: 3 } });
+    const amountInput = queryByTestId('amount');
+    const coupomAmountInput = queryByTestId('coupomAmount');
+    const priceInput = queryByTestId('price');
 
-  expect(amountInput.value).toBe('1');
-  expect(coupomAmountInput.value).toBe('3');
-  expect(priceInput.value).toBe('0');
+    fireEvent.change(amountInput, { target: { value: 7 } });
+    fireEvent.change(coupomAmountInput, { target: { value: 3 } });
+
+    expect(amountInput.value).toBe('7');
+    expect(coupomAmountInput.value).toBe('3');
+    expect(priceInput.value).toBe('55');
+  });
+
+  it('calculate price input with coupom value > total value', () => {
+    const { queryByTestId } = render(<PriceCalculator />);
+
+    const amountInput = queryByTestId('amount');
+    const coupomAmountInput = queryByTestId('coupomAmount');
+    const priceInput = queryByTestId('price');
+
+    fireEvent.change(amountInput, { target: { value: 1 } });
+    fireEvent.change(coupomAmountInput, { target: { value: 3 } });
+
+    expect(amountInput.value).toBe('1');
+    expect(coupomAmountInput.value).toBe('3');
+    expect(priceInput.value).toBe('0');
+  });
 });
